c0010c1cf3fe530a3de1674a1a463b44113dd70d
hahbr88 <hahbr88@hanmail.net>
2024-04-18 17:59:08 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center w-[200px] h-fit p-4 border border-solid border-cyan-600 rounded-md">
+        <div className="">
+          <div className="text-[24px] font-bold flex justify-center">
+            {count}
+          </div>
+          <div className="flex gap-2 mt-2">
+            <button
+              className="w-[80px] h-[60px] bg-cyan-950 text-white rounded-md hover:bg-cyan-600"
+              onClick={() => {
+                setCount((prev) => prev - 1);
+              }}>
+              -
+            </button>
+            <button
+              className="w-[80px] h-[60px] bg-cyan-950 text-white rounded-md hover:bg-cyan-600"
+              onClick={() => {
+                setCount((prev) => prev + 1);
+              }}>
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
