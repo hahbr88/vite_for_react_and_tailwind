1aa458da6893c9af7f4fa5420820da41c965461f
hahbr88 <hahbr88@hanmail.net>
2024-04-19 16:15:59 +0900

    Button 컴포넌트 임포트 수정, eslint 비활성화

--- src/App2.jsx
+++ src/App2.jsx
@@ -6,7 +6,7 @@ function App2() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex items-center justify-center h-screen">
       <div className="flex justify-center w-[200px] h-fit p-4 border border-solid border-cyan-600 rounded-md">
         <div className="">
           <div className="text-[24px] font-bold flex justify-center">
@@ -19,6 +19,7 @@ function App2() {
               {Button(setCount((prev) => prev - 1), "-")}
               {Button(setCount((prev) => prev + 1), "+")}
             */}
+            {/* 위와 같이 함수 호출 방식으로 쓰면 안됩니다. -병노- */}
           </div>
         </div>
       </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,17 @@
+/* eslint-disable react/prop-types */
 import "../App.css";
 
 //함수형 컴포넌트로 적으면 랜더링이 안 되는 것 같음 -> 왜?
 //function Button(onClick, children) {
-  
+
 //일급 함수를 이용하면 랜더링이 됨 -> 왜?
 const Button = ({ onClick, children }) => {
   return (
-   <button onClick={onClick} className="w-[80px] h-[60px] bg-red-500 text-white rounded-full hover:bg-red-550">
+    <button
+      onClick={onClick}
+      className="w-[80px] h-[60px] bg-red-500 text-white rounded-full hover:bg-red-550">
       {children}
-   </button>
+    </button>
   );
 };
 
