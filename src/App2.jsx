da96a02dba4f502ba581ac5164ac99f3eddb96fd
subin noh <binigy97@gmail.com>
2024-04-19 15:58:00 +0900

    Add : App2, Button

---
+++ src/App2.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "../../../vite_for_react_and_tailwind/vite_for_react_and_tailwind/src/components/Button";
+
+function App2() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex justi`fy-center items-center h-screen">
+      <div className="flex justify-center w-[200px] h-fit p-4 border border-solid border-cyan-600 rounded-md">
+        <div className="">
+          <div className="text-[24px] font-bold flex justify-center">
+            {count}
+          </div>
+          <div className="flex gap-2 mt-2">
+            <Button onClick={() => setCount((prev) => prev - 1)}>{"-"}</Button>
+            <Button onClick={() => setCount((prev) => prev + 1)}>{"+"}</Button>
+            {/*
+              {Button(setCount((prev) => prev - 1), "-")}
+              {Button(setCount((prev) => prev + 1), "+")}
+            */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App2;
