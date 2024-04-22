1a39ca5869ba8af080c2a10a46cccb1b8f16a3c8
hahbr88 <hahbr88@hanmail.net>
2024-04-22 15:16:04 +0900

    Custom Tag 컴포넌트 추가

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,75 +1,74 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
-import type { TableProps } from 'antd';
+import React, { useEffect, useState } from "react";
+import { Space, Table, Tag } from "antd";
+import type { TableProps } from "antd";
 
 interface DataType {
-  key:     number;
-  name:    string; //이름     : 홍길동
-  birth:   string; //생년월일 : 1999.01.01(만25세)
+  key: number;
+  name: string; //이름     : 홍길동
+  birth: string; //생년월일 : 1999.01.01(만25세)
   skGrade: string; //기술등급 : 특급
-  role:    string; //주역할   : 개발
-  skill:   string; //사용기술 : Java, C, ProC, 웹스퀘어
+  role: string; //주역할   : 개발
+  skill: string; //사용기술 : Java, C, ProC, 웹스퀘어
   updInfo: string; //최종수정 : 홍길동/2024.04.22
 }
 
-const columns: TableProps<DataType>['columns'] = [
+const CustomTag = ({ skGrade }) => {
+  const [color, setColor] = useState("");
+  useEffect(() => {
+    if (skGrade === "초급") {
+      setColor("green");
+    } else if (skGrade === "중급") {
+      setColor("geekblue");
+    } else if (skGrade === "고급") {
+      setColor("gold");
+    } else if (skGrade === "특급") {
+      setColor("magenta");
+    }
+  }, [skGrade]);
+  return (
+    <Tag color={color} key={skGrade}>
+      {skGrade}
+    </Tag>
+  );
+};
+
+const columns: TableProps<DataType>["columns"] = [
   {
-    title: '이름',
-    dataIndex: 'name',
-    key: 'name',
+    title: "이름",
+    dataIndex: "name",
+    key: "name",
     render: (text) => <a>{text}</a>,
   },
   {
-    title: '생년월일',
-    dataIndex: 'birth',
-    key: 'birth',
+    title: "생년월일",
+    dataIndex: "birth",
+    key: "birth",
   },
   {
-    title: '기술등급',
-    key: 'skGrade',
-    dataIndex: 'skGrade',
-    render: ({color}, { skGrade }) => (
-      <>
-        let {color} = '';
-        if(skGrade === '초급') {
-          color = 'green'
-        }
-        else if(skGrade === '중급') {
-          color = 'geekblue'
-        }
-        else if(skGrade === '고급') {
-          color = 'gold'
-        }
-        else if(skGrade === '특급') {
-          color = 'magenta'
-        }
-        return (
-          <Tag color={color} key={skGrade}>
-            {skGrade}
-          </Tag>
-        );
-      </>
-    ),
+    title: "기술등급",
+    key: "skGrade",
+    dataIndex: "skGrade",
+    render: ({ skGrade }) => <CustomTag skGrade={skGrade} />,
   },
   {
-    title: '주역할',
-    dataIndex: 'role',
-    key: 'role',
+    title: "주역할",
+    dataIndex: "role",
+    key: "role",
   },
   {
-    title: '사용기술',
-    dataIndex: 'skill',
-    key: 'skill',
+    title: "사용기술",
+    dataIndex: "skill",
+    key: "skill",
   },
   {
-    title: '최종수정',
-    dataIndex: 'updInfo',
-    key: 'updInfo',
+    title: "최종수정",
+    dataIndex: "updInfo",
+    key: "updInfo",
   },
   {
-    title: '',
-    dataIndex: 'action',
-    key: 'action',
+    title: "",
+    dataIndex: "action",
+    key: "action",
   },
   /*
   {
@@ -85,72 +84,72 @@ const columns: TableProps<DataType>['columns'] = [
   */
 ];
 
-
-
-
 const data: DataType[] = [
   {
-    key:     1,
-    name:    '최진희',
-    birth:   '1999.01.01(만26세)',
-    skGrade:    '특급',
-    role:    '개발',
-    skill:   'Java, C, ProC, 웹스퀘어, JQuery, Oracle, mySQL, IBM DB2, Spring, BMX',
-    updInfo: '홍길동/2024.04.22',
+    key: 1,
+    name: "최진희",
+    birth: "1999.01.01(만26세)",
+    skGrade: "특급",
+    role: "개발",
+    skill:
+      "Java, C, ProC, 웹스퀘어, JQuery, Oracle, mySQL, IBM DB2, Spring, BMX",
+    updInfo: "홍길동/2024.04.22",
   },
   {
-    key:     2,
-    name:    '김민경',
-    birth:   '1999.01.01(만26세)',
-    skGrade:    '특급',
-    role:    '개발',
-    skill:   'Java, C, ProC, 웹스퀘어, JQuery, Oracle, mySQL, IBM DB2, Spring, BMX',
-    updInfo: '홍길동/2024.04.22',
+    key: 2,
+    name: "김민경",
+    birth: "1999.01.01(만26세)",
+    skGrade: "특급",
+    role: "개발",
+    skill:
+      "Java, C, ProC, 웹스퀘어, JQuery, Oracle, mySQL, IBM DB2, Spring, BMX",
+    updInfo: "홍길동/2024.04.22",
   },
   {
-    key:     3,
-    name:    '이의용',
-    birth:   '1999.01.01(만26세)',
-    skGrade:    '특급',
-    role:    'PL',
-    skill:   'Java, C, ProC, 웹스퀘어, JQuery, Oracle, mySQL, IBM DB2, Spring, BMX',
-    updInfo: '홍길동/2024.04.22',
+    key: 3,
+    name: "이의용",
+    birth: "1999.01.01(만26세)",
+    skGrade: "특급",
+    role: "PL",
+    skill:
+      "Java, C, ProC, 웹스퀘어, JQuery, Oracle, mySQL, IBM DB2, Spring, BMX",
+    updInfo: "홍길동/2024.04.22",
   },
   {
-    key:     4,
-    name:    '홍길동',
-    birth:   '1999.01.01(만26세)',
-    skGrade:    '초급',
-    role:    '테스터',
-    skill:   'Java, Oracle, Spring, BMX',
-    updInfo: '홍길동/2024.04.22',
+    key: 4,
+    name: "홍길동",
+    birth: "1999.01.01(만26세)",
+    skGrade: "초급",
+    role: "테스터",
+    skill: "Java, Oracle, Spring, BMX",
+    updInfo: "홍길동/2024.04.22",
   },
   {
-    key:     5,
-    name:    '박경진',
-    birth:   '1999.01.01(만26세)',
-    skGrade:    '중급',
-    role:    '개발',
-    skill:   'Java, Oracle, Spring, BMX',
-    updInfo: '홍길동/2024.04.22',
+    key: 5,
+    name: "박경진",
+    birth: "1999.01.01(만26세)",
+    skGrade: "중급",
+    role: "개발",
+    skill: "Java, Oracle, Spring, BMX",
+    updInfo: "홍길동/2024.04.22",
   },
   {
-    key:     6,
-    name:    '이승원',
-    birth:   '1999.01.01(만26세)',
-    skGrade:    '중급',
-    role:    '개발',
-    skill:   'Java, Oracle, Spring, BMX',
-    updInfo: '홍길동/2024.04.22',
+    key: 6,
+    name: "이승원",
+    birth: "1999.01.01(만26세)",
+    skGrade: "중급",
+    role: "개발",
+    skill: "Java, Oracle, Spring, BMX",
+    updInfo: "홍길동/2024.04.22",
   },
   {
-    key:     7,
-    name:    '김영범',
-    birth:   '1999.01.01(만26세)',
-    skGrade:    '고급',
-    role:    'PM',
-    skill:   'Java, Oracle, Spring, BMX',
-    updInfo: '홍길동/2024.04.22',
+    key: 7,
+    name: "김영범",
+    birth: "1999.01.01(만26세)",
+    skGrade: "고급",
+    role: "PM",
+    skill: "Java, Oracle, Spring, BMX",
+    updInfo: "홍길동/2024.04.22",
   },
 ];
 
